3152ee50cae11b7836eeff8a47ff24d6fba62541
garrepi <jacgarrett18@gmail.com>
2026-05-23 13:58:03 -0700

    change path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
+  base: "/gpx-timeline/",
 });
